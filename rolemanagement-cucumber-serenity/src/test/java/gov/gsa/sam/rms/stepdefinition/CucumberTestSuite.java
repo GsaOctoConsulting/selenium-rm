70010bfae65841328d9354278ec2708c5fb98063
Shah Raiaan <3013142874@GSA.GOV>
2019-10-08 12:39:50 -0400

    test
    testing

--- rolemanagement-cucumber-serenity/src/test/java/gov/gsa/sam/rms/stepdefinition/CucumberTestSuite.java
+++ rolemanagement-cucumber-serenity/src/test/java/gov/gsa/sam/rms/stepdefinition/CucumberTestSuite.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(plugin = { "pretty" }, glue = {
 		"gov.gsa.sam.rms.stepdefinition" }, features = "src/test/resources/features/", dryRun = false, tags = {
-				"@RoleAssign","@1" })
+				"@RoleAssign","@2" })
 public class CucumberTestSuite {
 	// "@SystemAccountEmail","@9"
 	// "@T1Workspace","@32"
