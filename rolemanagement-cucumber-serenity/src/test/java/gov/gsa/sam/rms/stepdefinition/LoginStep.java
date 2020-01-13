3b56757276e8d72452f0f6a811997faa0f45b723
raiaan-shah <3013142874@GSA.GOV>
2020-01-13 17:10:43 -0500

    jan13p3

--- rolemanagement-cucumber-serenity/src/test/java/gov/gsa/sam/rms/stepdefinition/LoginStep.java
+++ rolemanagement-cucumber-serenity/src/test/java/gov/gsa/sam/rms/stepdefinition/LoginStep.java
@@ -37,8 +37,8 @@ public class LoginStep {
 
 		beforeScenario();
 		
-		SignInUtility.signIntoWorkspace(ConstantsAccounts.ROLE_ADMIN_USER_3, Constants.USERPASS,
-				ConstantsAccounts.ROLE_ADMIN_USER_3_SECRETKEY, Constants.USER_FED);
+		SignInUtility.signIntoWorkspace(ConstantsAccounts.FSD_AGENT_1, Constants.USERPASS,
+				ConstantsAccounts.FSD_AGENT_1_SECRETKEY, Constants.USER_FED);
 		
 		
 		/*SignInUtility.signIntoWorkspace(ConstantsAccounts.IAE_ADMIN, Constants.USERPASS,
