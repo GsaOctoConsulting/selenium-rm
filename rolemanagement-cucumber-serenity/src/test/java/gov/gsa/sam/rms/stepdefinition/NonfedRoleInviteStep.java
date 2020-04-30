4994ffe8e7f9a6aed51832264e348ec766bb36e1
raiaan-shah <40496402+raiaan-shah@users.noreply.github.com>
2020-04-30 17:32:22 -0400

    april30p2
    sdf

--- rolemanagement-cucumber-serenity/src/test/java/gov/gsa/sam/rms/stepdefinition/NonfedRoleInviteStep.java
+++ rolemanagement-cucumber-serenity/src/test/java/gov/gsa/sam/rms/stepdefinition/NonfedRoleInviteStep.java
@@ -7,18 +7,25 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import gov.gsa.sam.rms.pages.T1WorkspacePage;
+import gov.gsa.sam.rms.utilities.Constants;
+import gov.gsa.sam.rms.utilities.ConstantsAccounts;
+import gov.gsa.sam.rms.utilities.LaunchBrowserUtil;
+import gov.gsa.sam.rms.utilities.SignInUtility;
 
 public class NonfedRoleInviteStep {
 	private static Logger logger = LoggerFactory.getLogger(NonfedRoleInviteStep.class);
 
 	@Given("^_1nri nonfed admin logs in$")
 	public void _1nri_nonfed_admin_logs_in() throws Throwable {
-
+		SignInUtility.signIntoWorkspace(ConstantsAccounts.NONFED_ADMIN_ENTITYREGISTRATION, Constants.USERPASS,
+				ConstantsAccounts.NONFED_ADMIN_ENTITYREGISTRATION_SECRETKEY, Constants.USER_FED);
+		LaunchBrowserUtil.delay(4);
 	}
 
 	@And("^_1nri goes to the role invite page through user directory$")
 	public void _1nri_goes_to_the_role_invite_page_through_user_directory() throws Throwable {
-
+T1WorkspacePage.clickUserDirectoryLink();
 	}
 
 	@When("^_1nri admin enters an id for a user with roles in the admins own domain$")
