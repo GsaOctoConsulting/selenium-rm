cbc099584864a21ee5fcb02ad937174630e4d18d
raiaan-shah <40496402+raiaan-shah@users.noreply.github.com>
2020-07-22 13:34:28 -0400

    Revert "TestRunnerUpdated"
    This reverts commit 44cf04dd12a88c3819bdb2501105ba94ef3d5bbf.

--- rolemanagement-cucumber-serenity/src/test/java/gov/gsa/sam/rms/stepdefinition/CucumberTestSuite.java
+++ rolemanagement-cucumber-serenity/src/test/java/gov/gsa/sam/rms/stepdefinition/CucumberTestSuite.java
@@ -9,6 +9,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(plugin = { "pretty" }, glue = {
 		"gov.gsa.sam.rms.stepdefinition" }, features = "src/test/resources/features/", dryRun = false, tags = {
 				"@SystemAccount","@14"})
-public class RunCucumberTestSuite {
+public class CucumberTestSuite {
 // selecttrue
 }
