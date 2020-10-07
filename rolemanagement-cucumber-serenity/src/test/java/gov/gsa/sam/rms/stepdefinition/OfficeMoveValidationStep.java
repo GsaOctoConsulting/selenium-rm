9c652110669e09d35663016f055ecaab30f2814e
raiaan-shah <40496402+raiaan-shah@users.noreply.github.com>
2020-10-07 15:26:28 -0400

    p2

--- rolemanagement-cucumber-serenity/src/test/java/gov/gsa/sam/rms/stepdefinition/OfficeMoveValidationStep.java
+++ rolemanagement-cucumber-serenity/src/test/java/gov/gsa/sam/rms/stepdefinition/OfficeMoveValidationStep.java
@@ -1,5 +1,6 @@
 package gov.gsa.sam.rms.stepdefinition;
 
+import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,7 @@ import gov.gsa.sam.rms.pages.AssignRolePage;
 import gov.gsa.sam.rms.pages.MyRolesPage;
 import gov.gsa.sam.rms.pages.T1WorkspacePage;
 import gov.gsa.sam.rms.pages.UserDirectoryPage;
+import gov.gsa.sam.rms.pages.UserDirectoryViewAccessPage;
 import gov.gsa.sam.rms.utilities.Constants;
 import gov.gsa.sam.rms.utilities.ConstantsAccounts;
 import gov.gsa.sam.rms.utilities.FHUtility;
@@ -40,9 +42,10 @@ public class OfficeMoveValidationStep {
 //		String uniqueagencycode = "4" + counter.substring(1, counter.length());
 //		createdSubtier1 = FHUtility.createSubTier("RMSUBTIER" + uniqueagencycode, uniqueagencycode, "111", "111");
 	}
+
 	@And("^_1omv super admin creates an office under the first subtier$")
 	public void _1omv_super_admin_creates_an_office_under_the_second_subtier() throws Throwable {
-		//createdOfficeUnderSubtier1=FHUtility.createOffice("RMOFFICE"+counter,"47"+counter);
+		// createdOfficeUnderSubtier1=FHUtility.createOffice("RMOFFICE"+counter,"47"+counter);
 	}
 
 	@And("^_1omv super admin creates the second subtier under gsa$")
@@ -57,58 +60,90 @@ public class OfficeMoveValidationStep {
 //		LaunchBrowserUtil.closeBrowsers();
 	}
 
-	
-
 	@Then("^_1omv spaad logs in$")
 	public void _1omv_spaad_logs_in() throws Throwable {
-		SignInUtility.signIntoWorkspace(ConstantsAccounts.ROLE_ADMIN_USER_3, Constants.USERPASS,
-				ConstantsAccounts.ROLE_ADMIN_USER_3_SECRETKEY, Constants.USER_FED);
-		LaunchBrowserUtil.delay(4);
+//		SignInUtility.signIntoWorkspace(ConstantsAccounts.ROLE_ADMIN_USER_3, Constants.USERPASS,
+//				ConstantsAccounts.ROLE_ADMIN_USER_3_SECRETKEY, Constants.USER_FED);
+//		LaunchBrowserUtil.delay(4);
 	}
 
 	@And("^_1omv assigns contracting officer role in contract data at office level under subtier one$")
 	public void _1omv_assigns_contracting_officer_role_in_contract_data_and_contracting_opportunites_domain_to_a_user_at_office_level_under_subtiert_two()
 			throws Throwable {
-		LaunchBrowserUtil.scrollAllTheWayDown();
-		UserDirectoryWidgetUtility.clickUserDirectoryLink();
-		UserDirectoryPage.searchUserInUserPicker(ConstantsAccounts.NO_ROLE_USER_2);
-		UserDirectoryPage.clickAssignRole(ConstantsAccounts.NO_ROLE_USER_2);
-		AssignRolePage.selectOrgIfFound(createdOfficeUnderSubtier1, 0);
-		AssignRolePage.selectRoleIfFound(Constants.ROLE_CONTRACTING_OFFICER_PUBLISHER);
-		AssignRolePage.selectDomainIfFound(Constants.DOMAIN_CONTRACT_DATA);
-		//AssignRolePage.selectDomainIfFound(Constants.DOMAIN_CONTRACT_OPPORTUNITIES);
-		AssignRolePage.writeComment("test");
-		AssignRolePage.clickDone();
-		AssignRolePage.clickCloseButton();
-		LaunchBrowserUtil.delay(5);
+//		LaunchBrowserUtil.scrollAllTheWayDown();
+//		UserDirectoryWidgetUtility.clickUserDirectoryLink();
+//		UserDirectoryPage.searchUserInUserPicker(ConstantsAccounts.NO_ROLE_USER_2);
+//		UserDirectoryPage.clickAssignRole(ConstantsAccounts.NO_ROLE_USER_2);
+//		AssignRolePage.selectOrgIfFound(createdOfficeUnderSubtier1, 0);
+//		AssignRolePage.selectRoleIfFound(Constants.ROLE_CONTRACTING_OFFICER_PUBLISHER);
+//		AssignRolePage.selectDomainIfFound(Constants.DOMAIN_CONTRACT_DATA);
+//		//AssignRolePage.selectDomainIfFound(Constants.DOMAIN_CONTRACT_OPPORTUNITIES);
+//		AssignRolePage.writeComment("test");
+//		AssignRolePage.clickDone();
+//		AssignRolePage.clickCloseButton();
+//		LaunchBrowserUtil.delay(5);
 	}
 
 	@And("^_1omv spaad assigns contracting specialist role to the user under contract data domain under subtier2$")
 	public void _1omv_spaad_assigns_contracting_specialist_role_to_the_user_under_contract_data_domain()
 			throws Throwable {
-		MyRolesPage.clickAssignRoleButton();
-		AssignRolePage.selectOrgIfFound(createdSubtier2, 0);
-		AssignRolePage.selectRoleIfFound(Constants.ROLE_CONTRACTING_SPECIALIST_EDITOR);
-		AssignRolePage.selectDomainIfFound(Constants.DOMAIN_CONTRACT_DATA);
-		AssignRolePage.writeComment("test");
-		AssignRolePage.clickDone();
-		AssignRolePage.clickCloseButton();
-		LaunchBrowserUtil.delay(5);
-		LaunchBrowserUtil.closeBrowsers();
+//		MyRolesPage.clickAssignRoleButton();
+//		AssignRolePage.selectOrgIfFound(createdSubtier2, 0);
+//		AssignRolePage.selectRoleIfFound(Constants.ROLE_CONTRACTING_SPECIALIST_EDITOR);
+//		AssignRolePage.selectDomainIfFound(Constants.DOMAIN_CONTRACT_DATA);
+//		AssignRolePage.writeComment("test");
+//		AssignRolePage.clickDone();
+//		AssignRolePage.clickCloseButton();
+//		LaunchBrowserUtil.delay(5);
+//		LaunchBrowserUtil.closeBrowsers();
 	}
 
 	@When("^_1omv super admin moves the office into subtier2$")
 	public void _1omv_super_admin_moves_the_office_into_subtier_1() throws Throwable {
-		SignInUtility.signIntoWorkspace(ConstantsAccounts.FH_SUPER_ADMIN, Constants.USERPASS,
-		ConstantsAccounts.FH_SUPER_ADMIN_SECRETKEY, Constants.USER_FED);
-		T1WorkspacePage.gotoFHPage();
-		FHUtility.goToOrgDetails(createdSubtier1);
-		FHUtility.moveOfficeIntoSubtier(createdSubtier2);
+//		SignInUtility.signIntoWorkspace(ConstantsAccounts.FH_SUPER_ADMIN, Constants.USERPASS,
+//		ConstantsAccounts.FH_SUPER_ADMIN_SECRETKEY, Constants.USER_FED);
+//		T1WorkspacePage.gotoFHPage();
+//		FHUtility.goToOrgDetails(createdSubtier1);
+//		FHUtility.moveOfficeIntoSubtier(createdSubtier2);
 	}
 
 	@Then("^_1omv user should be left with one role$")
 	public void _1omv_user_should_be_left_with_one_role() throws Throwable {
+		SignInUtility.signIntoWorkspace(ConstantsAccounts.ROLE_ADMIN_USER_3, Constants.USERPASS,
+				ConstantsAccounts.ROLE_ADMIN_USER_3_SECRETKEY, Constants.USER_FED);
+		LaunchBrowserUtil.delay(4);
 
+		LaunchBrowserUtil.scrollAllTheWayDown();
+		UserDirectoryWidgetUtility.clickUserDirectoryLink();
+		UserDirectoryPage.searchUserInUserPicker(ConstantsAccounts.NO_ROLE_USER_2);
+		UserDirectoryPage.clickViewAccess(ConstantsAccounts.NO_ROLE_USER_2);
+		boolean rolefoundsubtier2 = UserDirectoryViewAccessPage.userHasRole(createdSubtier2,
+				Constants.ROLE_CONTRACTING_SPECIALIST_EDITOR, Constants.DOMAIN_CONTRACT_DATA, Constants.NOACTION);
+		Assert.assertEquals(true, rolefoundsubtier2);
+
+		UserDirectoryViewAccessPage.latestRoleHistoryFound("Due to organization re-alignment",
+				Constants.ROLEHISTORY_STATUS_ROLE_REMOVED, Constants.ROLE_CONTRACTING_OFFICER_PUBLISHER,
+				createdOfficeUnderSubtier1, Constants.NOACTION);
+		// delete the role and move the office back
+
+		boolean rolefoundsubtier = UserDirectoryViewAccessPage.userHasRole(createdSubtier2,
+				Constants.ROLE_CONTRACTING_SPECIALIST_EDITOR, Constants.DOMAIN_CONTRACT_DATA, Constants.DELETE);
+		Assert.assertEquals(true, rolefoundsubtier);
+		
+		// move the office back
+
+		LaunchBrowserUtil.delay(3);
+		
+		
+		
+		SignInUtility.signIntoWorkspace(ConstantsAccounts.FH_SUPER_ADMIN, Constants.USERPASS,
+		ConstantsAccounts.FH_SUPER_ADMIN_SECRETKEY, Constants.USER_FED);
+		T1WorkspacePage.gotoFHPage();
+		FHUtility.goToOrgDetails(createdSubtier2);
+		FHUtility.moveOfficeIntoSubtier(createdSubtier1);
+		
+		
+		
 	}
 
 }
