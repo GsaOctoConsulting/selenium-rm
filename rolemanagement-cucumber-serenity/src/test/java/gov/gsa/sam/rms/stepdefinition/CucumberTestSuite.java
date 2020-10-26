ad6c972634a3aac3ee0d31ee412c6e4d812d4537
raiaan-shah <40496402+raiaan-shah@users.noreply.github.com>
2020-10-25 21:53:34 -0400

    p2

--- rolemanagement-cucumber-serenity/src/test/java/gov/gsa/sam/rms/stepdefinition/CucumberTestSuite.java
+++ rolemanagement-cucumber-serenity/src/test/java/gov/gsa/sam/rms/stepdefinition/CucumberTestSuite.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(monochrome=true, plugin = { "pretty"}, glue = {
 		"gov.gsa.sam.rms.stepdefinition"}, features = "src/test/resources/features/", dryRun = false, tags = {
-				"@Email","@4"})
+				"@Email","@IntegrationTest"})
 public class CucumberTestSuite {
   // draft registration
 }
